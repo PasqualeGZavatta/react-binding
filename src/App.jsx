5327b86fc7ac15b6d1c6fad5c761041e4a678539
PasqualeGZavatta <pasqualeg.zavatta@gmail.com>
2026-09-23 14:37:18 +0200

    Start project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Footer from './components/layouts/Footer'
+import Header from './components/layouts/Header'
+import MainContent from './components/layouts/MainContent'
+
+function App() {
+
+
+  return (
+    <>
+      <Header />
+      <MainContent />
+      <Footer />
+    </>
+  )
+}
+
+export default App
